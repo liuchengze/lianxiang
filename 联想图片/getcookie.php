6509d443b78754d9eaa7405d423a310ce3706c6e
liuchengze <760319417@qq.com>
2017-06-28 21:18:56 +0800

    Add files via upload

---
+++ 联想图片/getcookie.php
@@ -0,0 +1,11 @@
+	function yfx_set_cookie(name, value, domain) {
+        var yfx_0j = "";
+		 if (window.document.cookie.length) {
+            yfx_0j = new Date((new Date()).getTime() + 24 * 360000 * 365 * 10);
+            yfx_0j = "; expires=" + yfx_0j.toGMTString();
+            window.document.cookie = name + "=" + escape(value) + yfx_0j + ";domain="+domain+";path=/;"
+        }
+    }
+var LA_C_C_Id = '_sk201703200607020.22990900.6773';
+lenovo_analytics('10000001',LA_C_C_Id);
+yfx_set_cookie('LA_C_C_Id','_sk201703200607020.22990900.6773','lenovo.com.cn');
